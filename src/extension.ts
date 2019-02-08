b02b051c6d86475adb6507775287b83bd3c46e12
Sergey Klevakin <justerest@yandex.ru>
2019-02-08 21:59:22 +0500

    in progress

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,27 @@
-import { commands, ExtensionContext, window } from 'vscode';
+import { multiReplaceFiles } from '@justerest/multi-replace';
+import { basename, extname } from 'path';
+import { commands, ExtensionContext, Uri, window } from 'vscode';
 
 export function activate(context: ExtensionContext) {
-    const disposable = commands.registerCommand('extension.helloWorld', () => {
-        window.showInformationMessage('Hello World!');
+    const multiReplaceHook = commands.registerCommand('extension.multiReplace', async (_uri?: Uri, uris?: Uri[]) => {
+        if (!uris) return window.showErrorMessage('Use explorer context menu');
+
+        const paths = uris.map((uri) => uri.fsPath);
+        const placeholder = basename(paths[0], extname(paths[0]));
+        const searchValue = await window.showInputBox({ prompt: 'Search value', value: placeholder, ignoreFocusOut: true });
+        if (!searchValue) return window.showInformationMessage('Multi replace canceled');
+        const replaceValue = await window.showInputBox({ prompt: 'Replace value', value: placeholder, ignoreFocusOut: true });
+        if (!replaceValue) return window.showInformationMessage('Multi replace canceled');
+
+        multiReplaceFiles({ paths, searchValue, replaceValue })
+            .subscribe({
+                complete() {
+                    window.showInformationMessage('Multi replace successfully completed');
+                },
+            });
     });
-    context.subscriptions.push(disposable);
+
+    context.subscriptions.push(multiReplaceHook);
 }
 
 export function deactivate() {
