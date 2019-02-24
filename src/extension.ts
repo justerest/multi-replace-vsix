cd97b438b744404ed0369ded2533c674777af083
justerest <justerest@yandex.com>
2019-02-24 21:16:53 +0500

    feat: add strict multi-replace (doesn't use case transformations)

--- src/extension.ts
+++ src/extension.ts
@@ -1,32 +1,45 @@
-import { multiReplace } from '@justerest/multi-replace';
+import { ChangedFileData, multiReplace, multiReplaceStrict } from '@justerest/multi-replace';
 import { basename, extname } from 'path';
+import { Observable } from 'rxjs';
 import { commands, ExtensionContext, Uri, window } from 'vscode';
 
 export function activate(context: ExtensionContext) {
-    const multiReplaceHook = commands.registerCommand('extension.multiReplace', async (_uri?: Uri, uris?: Uri[]) => {
-        if (!uris) return window.showErrorMessage('Use explorer context menu');
+  const multiReplaceHook = commands.registerCommand('extension.multiReplace', getCommand(multiReplace));
+  const multiReplaceStrictHook = commands.registerCommand('extension.multiReplaceStrict', getCommand(multiReplaceStrict));
 
-        const paths = uris.map((uri) => uri.fsPath);
-        const placeholder = basename(paths[0], extname(paths[0]));
-        const searchValue = await window.showInputBox({ prompt: 'Search value', value: placeholder, ignoreFocusOut: true });
-        if (!searchValue) return cancel();
-        const replaceValue = await window.showInputBox({ prompt: 'Replace value', value: placeholder, ignoreFocusOut: true });
-        if (!replaceValue) return cancel();
+  context.subscriptions.push(multiReplaceHook);
+  context.subscriptions.push(multiReplaceStrictHook);
+}
 
-        multiReplace(paths, searchValue, replaceValue).subscribe({
-            complete() {
-                window.showInformationMessage('Multi replace successfully completed');
-            },
-        });
+function getCommand(replace: (paths: string[], searchValue: string, replaceValue: string) => Observable<ChangedFileData>) {
+  return async (_uri?: Uri, uris?: Uri[]) => {
+    if (!uris) {
+      return window.showErrorMessage('Use explorer context menu');
+    }
+
+    const paths = uris.map((uri) => uri.fsPath);
+    const placeholder = basename(paths[0], extname(paths[0]));
+    const searchValue = await window.showInputBox({ prompt: 'Search value', value: placeholder, ignoreFocusOut: true });
+    if (!searchValue) {
+      return cancel();
+    }
+    const replaceValue = await window.showInputBox({ prompt: 'Replace value', value: placeholder, ignoreFocusOut: true });
+    if (!replaceValue) {
+      return cancel();
+    }
+
+    replace(paths, searchValue, replaceValue).subscribe({
+      complete() {
+        window.showInformationMessage('multi-replace successfully completed');
+      },
     });
-
-    context.subscriptions.push(multiReplaceHook);
+  };
 }
 
 function cancel() {
-    return window.showInformationMessage('Multi replace canceled');
+  return window.showInformationMessage('multi-replace canceled');
 }
 
 export function deactivate() {
-    //
+  //
 }
