72c4be08d6f9b6776c0b473bd3d5a8a49fc00e7d
justerest <justerest@gmailcom>
2019-08-15 23:20:26 +0500

    refactor: del unused commands

--- src/extension.ts
+++ src/extension.ts
@@ -1,43 +1,50 @@
-import { ChangedFileData, multiReplace, multiReplaceStrict } from '@justerest/multi-replace';
+import { multiReplace } from '@justerest/multi-replace';
 import { basename, extname } from 'path';
-import { Observable } from 'rxjs';
 import { commands, ExtensionContext, Uri, window } from 'vscode';
 
 export function activate(context: ExtensionContext) {
-  const multiReplaceHook = commands.registerCommand('extension.multiReplace', getCommand(multiReplace));
-  const multiReplaceStrictHook = commands.registerCommand('extension.multiReplaceStrict', getCommand(multiReplaceStrict));
-
+  const multiReplaceHook = commands.registerCommand('extension.multiReplace', multiReplaceCommand);
   context.subscriptions.push(multiReplaceHook);
-  context.subscriptions.push(multiReplaceStrictHook);
 }
 
-function getCommand(replace: (paths: string[], searchValue: string, replaceValue: string) => Observable<ChangedFileData>) {
-  return async (_uri?: Uri, uris?: Uri[]) => {
-    if (!uris) {
-      return window.showErrorMessage('Use explorer context menu');
-    }
-
+async function multiReplaceCommand(_uri?: Uri, uris?: Uri[]) {
+  try {
+    uris = parseUris(uris);
     const paths = uris.map((uri) => uri.fsPath);
     const placeholder = basename(paths[0], extname(paths[0]));
-    const searchValue = await window.showInputBox({ prompt: 'Search value', value: placeholder, ignoreFocusOut: true });
-    if (!searchValue) {
-      return cancel();
-    }
-    const replaceValue = await window.showInputBox({ prompt: 'Replace value', value: placeholder, ignoreFocusOut: true });
-    if (!replaceValue) {
-      return cancel();
-    }
-
-    replace(paths, searchValue, replaceValue).subscribe({
-      complete() {
-        window.showInformationMessage('multi-replace successfully completed');
-      },
-    });
-  };
+    const searchValue = await askSearchValue(placeholder);
+    const replaceValue = await askReplaceValue(placeholder);
+    multiReplace(paths, searchValue, replaceValue).subscribe({ complete });
+  } catch (error) {
+    window.showInformationMessage((error as Error).message || 'multi-replace canceled');
+  }
 }
 
-function cancel() {
-  return window.showInformationMessage('multi-replace canceled');
+function parseUris(uris?: Uri[]): Uri[] {
+  if (!uris) {
+    throw new Error('Use explorer context menu');
+  }
+  return uris;
+}
+
+async function askSearchValue(placeholder: string): Promise<string> {
+  const searchValue = await window.showInputBox({ prompt: 'Search value', value: placeholder, ignoreFocusOut: true });
+  if (searchValue) {
+    return searchValue;
+  }
+  throw new Error('');
+}
+
+async function askReplaceValue(placeholder: string): Promise<string> {
+  const replaceValue = await window.showInputBox({ prompt: 'Replace value', value: placeholder, ignoreFocusOut: true });
+  if (replaceValue) {
+    return replaceValue;
+  }
+  throw new Error('');
+}
+
+function complete() {
+  window.showInformationMessage('multi-replace successfully completed');
 }
 
 export function deactivate() {
